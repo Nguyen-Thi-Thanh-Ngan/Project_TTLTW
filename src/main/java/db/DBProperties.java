27c755d18ed6c86a1a2b7693ed024460dc1c7198
Linh2404XD <21130092@st.hcmuaf.edu.vn>
2024-06-07 16:49:07 +0700

    feat(giohang.jsp) : add function click checkbox select products to pay

--- src/main/java/db/DBProperties.java
+++ src/main/java/db/DBProperties.java
@@ -2,7 +2,7 @@ package db;
 public class DBProperties {
     public static final String HOST = "localhost";
     public static final String PORT = "3306";
-    public static final String DBNAME = "projectLTW";
+    public static final String DBNAME = "project_web";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "root";
+    public static final String PASSWORD = "";
 }
