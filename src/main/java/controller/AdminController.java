639570f74873099e442e390291c76ce3a29d1c2f
Linh2404XD <147931651+Linh2404XD@users.noreply.github.com>
2024-07-21 16:51:58 +0700

    feat: done function userpermission

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -13,8 +13,6 @@ public class AdminController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Đặt lại thuộc tính "fromHome" khi người dùng truy cập trang admin
-        request.getSession().setAttribute("fromHome", false);
 
         request.getRequestDispatcher("/admin.jsp").forward(request, response);
     }
